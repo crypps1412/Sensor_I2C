2d9a1a6b812e9f0fc6f16bd19390ca4edd926a21
crypps1412 <46725920+crypps1412@users.noreply.github.com>
2021-12-04 21:11:32 +0700

    Add files via upload

--- Sensor_I2C/src/Sensor_I2C.cpp
+++ Sensor_I2C/src/Sensor_I2C.cpp
@@ -4,16 +4,16 @@
 /********** I2C **********/
 
 /*
- * Get all device address on I2C bus
+ * Get all device addresses on I2C bus
  * Input:
  *  uint8_t array: to save address
- *  uint8_t &: number of the first devices you want to get address from (default: 128)
+ *  uint8_t: number of the first devices you want to get address from (default: 128)
  *  bool: check if you just want to get the number of devices (default: 0)
- * Output: uint8_t: number of devices whose address was read
+ * Output: uint8_t number of devices whose addresses was read
  */
-uint8_t I2C::scanAllDevices(uint8_t addr[], uint8_t maxNumberOfDevices, bool justNumber) {
+uint8_t I2C::scanAllDevices(uint8_t addr[], uint8_t maxDevices, bool justNumber) {
   uint8_t i = 0;
-  for (uint8_t a = 0; a < 128 && i < maxNumberOfDevices; a++) {
+  for (uint8_t a = 0; a < 128 && i < maxDevices; a++) {
     Wire.beginTransmission(a);
     if (!Wire.endTransmission())
       if (justNumber) i++;
@@ -48,13 +48,18 @@ uint8_t I2C::numberOfDevices() {
 
 /*
  * Check if the address provided to code is correct
- * Input: uint8_t &: the address provided
- * Output: bool: 1 if connection is good, 0 otherwise
+ * Input: uint8_t & the address providing
+ * Output: uint8_t:
+ * 	 0 if connection is good
+ *   1 if full buffer
+ *   2 if address sent, NACK received
+ *   3 if data sent, NACK received
+ *   4 for other error
  */
-bool I2C::checkDeviceConnection(const uint8_t &addr) {
+uint8_t I2C::checkDeviceConnection(const uint8_t &addr) {
   if (addr > 127) return 0;
   Wire.beginTransmission(addr);
-  return !Wire.endTransmission();
+  return Wire.endTransmission();
 }
 
 /*
@@ -68,16 +73,16 @@ void I2C::flush() {
 
 
 /********** SENSOR **********/
+
 /*
  * Check device address and connection, if provided address fails to response, automatically
  * find address following ordinal
  * Input:
- *  uint8_t: the provided address (default: 0)
+ *  uint8_t: the provided address (default: 255)
  *  uint8_t: ordinal (default: 1)
- * Output: bool: have to success to continue the program (always 1)
+ * Output: bool have to success to continue the program (always 1)
  */
 bool SENSOR::sensorBegin(uint8_t addr, uint8_t ordinal) {
-  Wire.begin();
   if (addr > 127) {
     addr = I2C::scanDeviceByOrdinal(ordinal);
     if (addr == 255) {
@@ -85,17 +90,22 @@ bool SENSOR::sensorBegin(uint8_t addr, uint8_t ordinal) {
       delay(999);
       return sensorBegin();
     }
-  } else if (!I2C::checkDeviceConnection(addr)) {
+  } else if (I2C::checkDeviceConnection(addr)) {
       Serial.println(F("Can't find device with provided address! Auto-scanning!!!"));
       return sensorBegin();
   }
-  this->_id = getByte(_idAddr);
+  this->_id = readByte(_idAddr);
   this->_addr = addr;
   Serial.print(F("Detected "));
   this->showInfo();
   return 1;
 }
 
+/*
+ * Print sensor info (name, address, id) to serial
+ * Input: none
+ * Output: none
+ */
 void SENSOR::showInfo() {
   Serial.println(this->_name);
   Serial.print(F("Device Address: 0b"));
@@ -108,15 +118,33 @@ void SENSOR::showInfo() {
   Serial.println(this->_id, HEX);
 }
 
+/*
+ * Get sensor name
+ * Input: none
+ * Output: char* name pointer
+ */
 char* SENSOR::getName() {
   return _name;
 }
 
+/*
+ * Get sensor id
+ * Input: none
+ * Output: uint8_t id number
+ */
 uint8_t SENSOR::getId() {
   return _id;
 }
 
-bool SENSOR::getBytes(int8_t bytes[], const uint8_t &numberOfBytes, int8_t failAlt) {
+/*
+ * Read multiple bytes from I2C sensor
+ * Input:
+ *  uint8_t[]: saving array, with the first element containing register address
+ *  uint8_t &: number of bytes to collect
+ *  uint8_t: result when failing to read (default: 0)
+ * Output: bool true if reading successfully
+ */
+bool SENSOR::readBytes(uint8_t bytes[], const uint8_t &numberOfBytes, uint8_t failAlt) {
   Wire.beginTransmission(_addr);
   Wire.write(bytes[0]);
   if (!Wire.endTransmission()) {
@@ -130,19 +158,40 @@ bool SENSOR::getBytes(int8_t bytes[], const uint8_t &numberOfBytes, int8_t failA
   return false;
 }
 
-uint8_t SENSOR::getByte(const uint8_t &registerAddr, uint8_t failAlt) {
+/*
+ * Read a byte from I2C sensor
+ * Input:
+ *  uint8_t &: reading register address
+ *  uint8_t: result when failing to read (default: 0)
+ * Output: uint8_t data read from register
+ */
+uint8_t SENSOR::readByte(const uint8_t &registerAddr, uint8_t failAlt) {
   uint8_t data[] = {registerAddr};
-  this->getBytes(data, 1, failAlt);
+  this->readBytes(data, 1, failAlt);
   return *data;
 }
 
-uint16_t SENSOR::getWord(const uint8_t &registerAddr, uint16_t failAlt) {
+/*
+ * Read 2 bytes from I2C sensorl
+ * Input:
+ *  uint8_t &: reading registers address
+ *  uint16_t: result when failing to read (default: 0)
+ * Output: uint16_t data read from 2 continual registers
+ */
+uint16_t SENSOR::readWord(const uint8_t &registerAddr, uint16_t failAlt) {
   uint8_t data[2] = {registerAddr, 0};
-  if (!this->getBytes(data, 2)) return failAlt;
+  if (!this->readBytes(data, 2)) return failAlt;
   return data[0] << 8 | data[1];
 }
 
-bool SENSOR::sendByte(const uint8_t &registerAddr, const uint8_t &cwd) {
+/*
+ * Write a byte to a register on I2C sensor
+ * Input:
+ *  uint8_t &: writing registers address
+ *  uint8_t &: control word
+ * Output: bool true if writing successfully
+ */
+bool SENSOR::writeByte(const uint8_t &registerAddr, const uint8_t &cwd) {
   Wire.beginTransmission(_addr);
   Wire.write(registerAddr);
   Wire.write(cwd);
--- Sensor_I2C/src/Sensor_I2C.h
+++ Sensor_I2C/src/Sensor_I2C.h
@@ -5,26 +5,26 @@
 #include <Wire.h>
 
 namespace I2C {
-uint8_t scanAllDevices(uint8_t address[], uint8_t maxNumberOfDevices = 128, bool justNumber = 0);
-uint8_t scanDeviceByOrdinal(uint8_t ordinal = 1);
-uint8_t numberOfDevices();
-bool checkDeviceConnection(const uint8_t &addr);
-void flush();
+uint8_t scanAllDevices(uint8_t addr[], uint8_t maxDevices = 128, bool justNumber = 0);	// Get number and addresses of all I2C devices
+uint8_t scanDeviceByOrdinal(uint8_t ordinal = 1);	// Get I2C device address by ordinal
+uint8_t numberOfDevices();	// Get number of I2C devices
+uint8_t checkDeviceConnection(const uint8_t &addr);	// Check I2C device connection
+void flush();	// Flush all unused data left in buffer
 }
 
 class SENSOR {
   public:
-    SENSOR(const char *name, const char &idAddr): _idAddr(idAddr), _name(name) {};
-    bool sensorBegin(uint8_t addr = 255, uint8_t ordinal = 1);
+    SENSOR(const char *deviceName, const uint8_t &idAddr): _idAddr(idAddr), _name(deviceName) {};	// Set sensor name and Id address
+    bool sensorBegin(uint8_t addr = 255, uint8_t ordinal = 1);	// Check sensor address by user provided address or ordinal
 
-    void showInfo();
-    char* getName();
-    uint8_t getId();
+    void showInfo();	// Print to serial sensor name, address and Id
+    char* getName();	// Get sensor name pointer
+    uint8_t getId();	// Get sensor Id
 
-    bool getBytes(int8_t data[], const uint8_t &numberOfBytes, int8_t failAlt = 0);
-    uint8_t getByte(const uint8_t &registerAddr, uint8_t failAlt = 0);
-    uint16_t getWord(const uint8_t &registerAddr, uint16_t failAlt = 0);
-    bool sendByte(const uint8_t &registerAddr, const uint8_t &cwd);
+    bool readBytes(uint8_t bytes[], const uint8_t &numberOfBytes, uint8_t failAlt = 0);	// Read a number of bytes
+    uint8_t readByte(const uint8_t &registerAddr, uint8_t failAlt = 0);	// Read a byte
+    uint16_t readWord(const uint8_t &registerAddr, uint16_t failAlt = 0);	// Read 2 bytes
+    bool writeByte(const uint8_t &registerAddr, const uint8_t &cwd);	// Write a byte to a register
   private:
     uint8_t _addr = 0, _idAddr, _id = 0;
     char* _name;
